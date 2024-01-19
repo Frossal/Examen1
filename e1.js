e0a8658023912edacb17572caf3d3968eb5a04f7
Frossal <156116218+Frossal@users.noreply.github.com>
2024-01-19 11:39:14 -0500

    Add files via upload

---
+++ e1.js
@@ -0,0 +1,18 @@
+
+
+
+function ponerx(id) {  
+	var uno = document.getElementById(id);
+  if (uno.textContent == '') 
+  		uno.textContent = 'x';
+  else uno.textContent = ''; 
+
+}
+
+
+
+
+
+
+
+
